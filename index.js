2a4ef302462a4bf05ae98946659299e6b87f48c4
AttAditya <attachment.aditya@gmail.com>
2024-07-07 00:40:39 +0530

    Created a simple API server, with PostWoman for API testing in future.

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const fs = require("fs");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const chatsRouter = require("./src/routes/chats.route");
+
+app.use("/api/chats", chatsRouter);
+
+app.get("/postwoman", (req, res) => {
+    let postwomanHtmlPath = "./src/views/postwoman.html";
+    let postwomanHtml = fs.readFileSync(postwomanHtmlPath, "utf8");
+    res.send(postwomanHtml);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
+
